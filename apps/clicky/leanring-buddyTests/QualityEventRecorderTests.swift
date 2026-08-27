375c19bfc8788606a604b191cb03875e577fe8ef
yishu-ziyu <yishuziyu@gmail.com>
2026-08-27 10:35:32 +0800

    fix: import Clicky in QualityEventRecorderTests
    CI failed because PRODUCT_MODULE_NAME is Clicky, not leanring_buddy.

--- apps/clicky/leanring-buddyTests/QualityEventRecorderTests.swift
+++ apps/clicky/leanring-buddyTests/QualityEventRecorderTests.swift
@@ -1,5 +1,5 @@
 import XCTest
-@testable import leanring_buddy
+@testable import Clicky
 
 final class QualityEventRecorderTests: XCTestCase {
     func testRejectsTranscriptAttributesByIgnoringTheWrite() {
